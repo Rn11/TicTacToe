9773efecd4b28140ca5d9f2bf4fdc0ee55312e36
Rn11 <mmln2007@yahoo.de>
2016-12-20 18:47:05 +0100

    Update Computer.java

--- TicTacToe/src/ticTacToe/Computer.java
+++ TicTacToe/src/ticTacToe/Computer.java
@@ -6,12 +6,16 @@ public class Computer {
 	public static int move(String[] spielfeld) {
 		Random rn = new Random();
 		ArrayList<Integer> freieFelder = new ArrayList<Integer>();
+		//Durchlaufe alle Felder des Spielfelds
 		for (int i = 0; i < spielfeld.length; i++) {
-
+			//wenn Spielfeld an Position 'i' frei ist
 			if (spielfeld[i] == " ") {
+				//füge den Wert vom aktuellen Index an die ArrayList
 				freieFelder.add(i);
 			}
 		}
+		//Gebe einen zufällig gewählten Wert aus der ArrayList zurück
+		//hierfür wird eine Zufallszahl zwischen 0 und der Größe der ArrayList als Index ermittelt
 		return freieFelder.get(rn.nextInt(freieFelder.size()));
 	}
 }
